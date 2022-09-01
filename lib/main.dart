d436e8dd07c1734e26e24555017f17a7f273cde1
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-09-01 18:20:18 +0300

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:food_ordering_app/firebase_options.dart';
 import 'package:food_ordering_app/provider/counter_provider.dart';
 import 'package:food_ordering_app/src/app.dart';
@@ -10,8 +11,15 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterNativeSplash.removeAfter(initialization);
+
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (context) => Counter())],
     child: const MyApp(),
   ));
+}
+
+Future initialization(BuildContext context) async {
+  await Future.delayed(Duration(seconds: 3));
 }
